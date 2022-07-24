50c918bf2cc6ef8cccb5e1dd204790e33a25e022
Jonashio <jonashio.es@gmail.com>
2022-07-24 11:37:43 +0200

    Fix fail when I created the project. ( storyboard -> swiftui )

---
+++ TripManagerSwiftUI/TripManagerSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TripManagerSwiftUIApp.swift
+//  TripManagerSwiftUI
+//
+//  Created by Jonashio on 24/7/22.
+//
+
+import SwiftUI
+
+@main
+struct TripManagerSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
